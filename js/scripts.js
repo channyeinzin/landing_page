0f3744b6cedaf9fc1bf0676503939b0e8b18fc75
Noel <nz2347@columbia.edu>
2024-07-26 11:33:15 -0400

    status fixing

--- js/scripts.js
+++ js/scripts.js
@@ -158,8 +158,7 @@ particlesJS("particles-js", {
     "retina_detect":true
   });
   
-  var count_particles, 
-      stats, update; 
+  var count_particles, stats, update; 
   stats = new Stats; 
   stats.setMode(0); 
   stats.domElement.style.position = 'absolute'; 
